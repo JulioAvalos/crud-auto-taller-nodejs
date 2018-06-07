4d459e032ef0210fdde903b1af732d00a78dcf30
Julio Avalos <julioavalos2@msn.com>
2018-06-07 00:53:35 -0600

    second commit

--- taller/server.js
+++ taller/server.js
@@ -1,6 +1,6 @@
 
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://javalos:admin1234@ds151180.mlab.com:51180/taller_auto');
+mongoose.connect(process.env.DATABASE_URL);
 
 var express = require('express');
 var bodyParser = require('body-parser');
@@ -9,7 +9,7 @@ var app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT;
 var router = express.Router();
 var Taller = require('./app/models/taller');
 
@@ -22,7 +22,7 @@ router.use(function (req, res, next) {
     console.log('Something is happening.');
     next(); // make sure we go to the next routes and don't stop here
 });
-
+ 
 
 router.route("/talleres")
     //get all workshops availables!
